e4ba458cde697092675242289fdfaf7a17862a47
Gustavo Petri <gpetri@irif.fr>
2019-07-24 17:23:48 +0100

    GP: Small change for target changes

--- rust/build-musl/README.md
+++ rust/build-musl/README.md
@@ -1,6 +1,10 @@
 Setup a VM and compile the project using cross
 ======================
 
+## Setup an evironment variable for armour targets
+
+    $ export ARMOUR_TARGET_DIR ~/armour-target
+
 ## Setup a VM named build-musl
 
     $ sh setup.sh
--- rust/build-musl/setup.sh
+++ rust/build-musl/setup.sh
@@ -4,3 +4,4 @@ vagrant up
 vagrant ssh -c 'sh install-rustup.sh -y'
 vagrant ssh -c 'cargo install cross'
 vagrant ssh -c 'cd rust/build-musl; docker build -t musl .'
+vagrant ssh -c 'echo "export CARGO_TARGET_DIR='/home/vagrant/armour-target'" >> /home/vagrant/.profile'
