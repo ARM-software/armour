547a47821efec5ab9db0adaef8b7105b8af94af6
Gustavo Petri <gpetri@irif.fr>
2019-07-25 10:36:32 +0100

    GP:

--- rust/build-musl/compile.sh
+++ rust/build-musl/compile.sh
@@ -1,3 +1,5 @@
 #! /bin/sh
 
 vagrant ssh -c 'cd rust/; cross build --release --target x86_64-unknown-linux-musl'
+vagrant ssh -c 'mkdir -p ~/rust/target/bins/'
+vagrant ssh -c 'find ~/rust/target/x86_64-unknown-linux-musl/release -maxdepth 1 -type f ! -name "*.*" -exec test -x {} \; -exec cp -ft /home/vagrant/rust/target/bins/ {} +'
