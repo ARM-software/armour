a79fbc2bff231bf4d748ecdafd0b251ab18c7c09
Basma El Gaabouri <basma.elgaabouri@arm.com>
2019-07-25 15:19:55 +0100

    poc iptales

--- PoC/vagrant-poc/iptables-generate.sh
+++ PoC/vagrant-poc/iptables-generate.sh
@@ -13,15 +13,18 @@ done
 sudo iptables -t nat -I PREROUTING -i poc_notif -p tcp --dport 80 -j DNAT --to-destination $proxy_ip:5000
 sudo iptables -t nat -I PREROUTING -i poc_pulse -p tcp --dport 4713 -j DNAT --to-destination $proxy_ip:5000
 sudo iptables -t nat -I PREROUTING -i poc_mongo-web -p tcp --dport 81 -j DNAT --to-destination $proxy_ip:5000
+sudo iptables -t nat -I PREROUTING -i poc_mongo -p tcp --dport 27017 -j DNAT --to-destination $proxy_ip:5000
 
 for i in $interfaces; do
-  if [[ $i != "poc_mongo-web" && $i != "poc_notif" && $i != "poc_pulse"  $i != "poc_mdebug" && $i != "poc_trust" && $i != "poc_public" ]] then
+  if [ "$i" != "poc_mongo-web" ] && [ "$i" != "poc_notif" ] && [ "$i" != "poc_pulse" ] && [ "$i" != "poc_mdebug" ] && [ "$i" != "poc_trust" ] && [ "$i" != "poc_public" ]
+  then
     sudo iptables -t nat -I PREROUTING -i $i -p tcp -j DNAT --to-destination $proxy_ip:5000
   fi
 done
 
 for i in $interfaces; do
-  if [[ $i == "poc_mdebug" || $i == "poc_trust" || $i == "poc_public" ]] then
+  if [ "$i" = "poc_mdebug" ] || [ "$i" = "poc_trust" ] || [ "$i" = "poc_public" ]
+  then
     sudo iptables -t nat -I PREROUTING -i $i -p tcp --match multiport --dport 1883,8883,9001 -j DNAT --to-destination $proxy_ip:5001
   fi
 done
