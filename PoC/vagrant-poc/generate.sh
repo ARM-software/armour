c6bde65754380f331ed7fe1893f33148f548be5a
Basma El Gaabouri <basma.elgaabouri@arm.com>
2019-07-26 12:58:40 +0100

    poc iptables

---
+++ PoC/vagrant-poc/generate.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+proxy_ip=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' armour-data`
+rest_port=5000
+tcp_port=5001
+rest_endpoints=(172.39.0.2:80 172.38.0.2:81 172.37.0.2:27017 172.36.0.2:5000 172.35.0.2:5000 172.34.0.2:5000 172.33.0.2:3306 172.32.0.2:5000 172.31.0.2:5000 172.30.0.2:5000 172.29.0.2:5000 172.28.0.2:5000 172.24.0.2:5000 172.23.0.2:5000 172.21.0.2:5000 172.20.0.2:5000 172.19.0.2:5000 172.18.0.2:5000)
+tcp_endpoints=(172.27.0.2:1883 172.27.0.2:8883 172.27.0.2:9001 172.26.0.2:1883 172.26.0.2:8883 172.26.0.2:9001 172.25.0.2:1883 172.25.0.2:8883 172.25.0.2:9001 172.22.0.2:4713)
+
+sudo iptables -I FORWARD -i poc_+ -o proxy-net -j DOCKER-USER
+sudo iptables -I FORWARD -i proxy-net -o poc_+ -j DOCKER-USER
+
+sudo iptables -I DOCKER-USER -i poc_+ -o proxy-net -j ACCEPT
+sudo iptables -I DOCKER-USER -i proxy-net -o poc_+ -j ACCEPT
+
+for i in "${rest_endpoints[@]}"; do
+  IFS=':' read -ra ports <<< "$i"
+  sudo iptables -t nat -I PREROUTING -i poc_+ -d ${ports[0]} --dport ${ports[1]} -j DNAT --to-destination $proxy_ip:$rest_port
+done
+
+for i in "${tcp_endpoints[@]}"; do
+  IFS=':' read -ra ports <<< "$i"
+  sudo iptables -t nat -I PREROUTING -i poc_+ -d ${ports[0]} --dport ${ports[1]} -j DNAT --to-destination $proxy_ip:$tcp_port
+  let "tcp_port++"
+done
