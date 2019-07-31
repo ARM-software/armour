72514974b7710e0d44a71c40f2ee92ebc5451212
Basma El Gaabouri <basma.elgaabouri@arm.com>
2019-07-31 10:08:19 +0000

    Delete iptables-allow.sh

--- PoC/vagrant-poc/iptables-allow.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-interfaces=`ip link | cut -d: -f2 | grep poc`
-for i in $interfaces; do
-  for j in $interfaces; do
-    sudo iptables -I DOCKER-USER -i $i -o $j -j ACCEPT
-  done
-done
