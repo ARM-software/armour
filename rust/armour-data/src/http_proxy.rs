64793edfe7cd5adfaf4d5272aa47db5bb4a32a69
Anthony Fox <anthony.fox@arm.com>
2019-07-31 14:49:44 +0100

    restore original "allow all" path

--- rust/armour-data/src/http_proxy.rs
+++ rust/armour-data/src/http_proxy.rs
@@ -189,51 +189,25 @@ pub fn request(
                 }),
         ),
         // allow client payload without check
-        Policy::AllowAll { debug } => {
-            future::Either::B(future::Either::A(
-                payload
-                    .from_err()
-                    .fold(web::BytesMut::new(), |mut body, chunk| {
-                        body.extend_from_slice(&chunk);
-                        Ok::<_, Error>(body)
-                    })
-                    .and_then(move |client_payload| {
-                        req.forward_url(*proxy_port).and_then(move |url| {
-                            let client_request = client
-                                .request_from(url.as_str(), req.head())
-                                .process_headers(req.peer_addr());
-                            if debug {
-                                debug!("{:?}", client_request)
-                            };
-                            client_request
-                                // forward the request (with the original client payload)
-                                .send_body(client_payload)
-                                // send the response back to the client
-                                .then(|res| response(p, policy, res))
-                        })
-                    }),
-            ))
-        }
-        Policy::Policy {
+        Policy::AllowAll { debug }
+        | Policy::Policy {
             client_payload: false,
             debug,
             ..
         } => {
-            future::Either::B(future::Either::B(req.forward_url(*proxy_port).and_then(
-                move |url| {
-                    let client_request = client
-                        .request_from(url.as_str(), req.head())
-                        .process_headers(req.peer_addr());
-                    if debug {
-                        debug!("{:?}", client_request)
-                    };
-                    client_request
-                        // forward the request (with the original client payload)
-                        .send_stream(payload)
-                        // send the response back to the client
-                        .then(|res| response(p, policy, res))
-                },
-            )))
+            future::Either::B(req.forward_url(*proxy_port).and_then(move |url| {
+                let client_request = client
+                    .request_from(url.as_str(), req.head())
+                    .process_headers(req.peer_addr());
+                if debug {
+                    debug!("{:?}", client_request)
+                };
+                client_request
+                    // forward the request (with the original client payload)
+                    .send_stream(payload)
+                    // send the response back to the client
+                    .then(|res| response(p, policy, res))
+            }))
         }
     }
 }
