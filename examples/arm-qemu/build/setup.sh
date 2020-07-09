e99a205986c4863f3513fbc76d96a56e3ef9a036
Anthony Fox <anthony.fox@arm.com>
2020-07-09 14:30:39 +0100

    optimise binaries for space in arm-qemu example

--- examples/arm-qemu/build/setup.sh
+++ examples/arm-qemu/build/setup.sh
@@ -5,7 +5,7 @@ source $HOME/.cargo/env
 cargo install cross
 docker build -t arm-unknown-linux-musleabihf .
 cd ~/src
-cross build --target=arm-unknown-linux-musleabihf -p armour-proxy
-cross build --target=arm-unknown-linux-musleabihf -p armour-host
+cross build --target=arm-unknown-linux-musleabihf -p armour-proxy --release
+cross build --target=arm-unknown-linux-musleabihf -p armour-host --release
 mkdir ~/bin
-ln -s ~/src/target/arm-unknown-linux-musleabihf/debug/{armour-proxy,armour-host} ~/bin
+ln -s ~/src/target/arm-unknown-linux-musleabihf/release/{armour-proxy,armour-host} ~/bin
