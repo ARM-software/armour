9b1e7e2f1152d28ef0cd3dff05d6d95e11398992
Anthony Fox <anthony.fox@arm.com>
2020-07-28 14:44:17 +0100

    "local host" -> "localhost"

--- examples/setup.sh
+++ examples/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 cd ../src
-ARMOUR_PASS=armour cargo run -p armour-certs -- --dir ../examples/certificates --control localhost 127.0.0.1 10.0.2.2 --host local host 127.0.0.1 10.0.2.2
+ARMOUR_PASS=armour cargo run -p armour-certs -- --dir ../examples/certificates --control localhost 127.0.0.1 10.0.2.2 --host localhost 127.0.0.1 10.0.2.2
 cd - > /dev/null
 cd control-plane
 ln -sf ../certificates .
